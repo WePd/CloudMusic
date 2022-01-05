a2d5fbbf9006acf197bf9321a73d7eebb5dd0c7e
Wepd <weped_x@163.com>
2022-01-05 16:56:52 +0800

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,43 +1,38 @@
-import React, { memo } from 'react'
+import React, { memo } from "react"
 
-import { NavLink } from 'react-router-dom'
-import { HeaderWrapper, HeaderLeft, HeaderRight } from './style'
-import { headerLinks } from '../../common/head-data'
+import { NavLink } from "react-router-dom"
+import { HeaderWrapper, HeaderLeft, HeaderRight } from "./style"
+import { headerLinks } from "../../common/head-data"
 
 export default memo(function Header() {
-  const show = (item, index) => {
-    if (index < 3) {
-      return (
-        <NavLink to={item.link}>{item.title}</NavLink >
-      )
-    } else {
-      return (
-        <a href={item.link}>{item.title}</a>
-      )
-    }
-  }
+	const show = (item, index) => {
+		if (index < 3) {
+			return <NavLink to={item.link}>{item.title}</NavLink>
+		} else {
+			return <a href={item.link}>{item.title}</a>
+		}
+	}
 
-  return (
-    <HeaderWrapper>
-      <div className='headerItem wrap-v1'>
-        <HeaderLeft>
-          <a href="#/" className="sprite_01"> </a>
-          <div className="select-list">
-            {
-              headerLinks.map((item, index) => {
-                return (
-                  <div key={item.index}>
-                    {show(item, index)}
-                  </div>
-                )
-              })
-            }
-          </div>
-        </HeaderLeft>
-        <HeaderRight>right</HeaderRight>
-      </div>
-      <div className='divder'></div>
-
-    </HeaderWrapper>
-  )
+	return (
+		<HeaderWrapper>
+			<div className="headerItem wrap-v1">
+				<HeaderLeft>
+					<a href="#/" className="logo sprite_01 ">
+						{" "}
+					</a>
+					<div className="select-list">
+						{headerLinks.map((item, index) => {
+							return (
+								<div key={index} className="select-item">
+									{show(item, index)}
+								</div>
+							)
+						})}
+					</div>
+				</HeaderLeft>
+				<HeaderRight>right</HeaderRight>
+			</div>
+			<div className="divder"></div>
+		</HeaderWrapper>
+	)
 })
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,37 +1,73 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
 export const HeaderWrapper = styled.div`
-  height: 75px;
-  background: #242424;
+	height: 75px;
+	background: #242424;
 
-  .headerItem {
-    height: 70px;
-    display: flex;
-    justify-content: space-between;
-  }
-  .divder{
-    height: 5px;
-    background: #c20c2c;
-  }
+	.headerItem {
+		height: 70px;
+		display: flex;
+		justify-content: space-between;
+	}
+	.divder {
+		height: 5px;
+		background: #c20c2c;
+	}
 `
 
 export const HeaderLeft = styled.div`
-  display: flex;
+	display: flex;
+	.logo {
+		width: 157px;
+		height: 69px;
+		text-indent: -9999px;
+		background-position: 0 0;
+	}
+	.select-list {
+		display: flex;
+		line-height: 70px;
 
-  a {
-    display: block;
-    width: 157px;
-    height: 100%;
-    padding-right: 20px;
-    background-position: 0 0;
-  }
-  .select-list {
-    display: flex;
-    line-hight: 70px;
-    
-  }
+		.select-item {
+			position: relative;
+
+			a {
+				display: block;
+				padding: 0 20px !important;
+				color: #ccc;
+			}
+
+			:last-of-type a {
+				position: relative;
+				::after {
+					position: absolute;
+					content: "";
+					width: 28px;
+					height: 19px;
+					background-image: url(${require("../../assets/img/sprite_01.png").default});
+					background-position: -190px 0;
+					top: 20px;
+					right: -15px;
+				}
+			}
+
+			&:hover a,
+			a.active {
+				color: #fff;
+				background: #000;
+				text-decoration: none;
+			}
+
+			.active .icon {
+				position: absolute;
+				display: inline-block;
+				width: 12px;
+				height: 7px;
+				bottom: -1px;
+				left: 50%;
+				transform: translate(-50%, 0);
+				background-position: -226px 0;
+			}
+		}
+	}
 `
-export const HeaderRight = styled.div`
-
-
-`
+export const HeaderRight = styled.div``
